892b42903a387a26f62eccce8f3e588d1128af04
alvolex <alexandervolkers@gmail.com>
2023-07-19 20:20:08 +0200

    fix name and types file

--- get-window-position.d.ts
+++ get-window-position.d.ts
@@ -1,3 +1,3 @@
-declare module 'adon' {
+declare module 'get-window-position' {
   export function getWindowPositionByName(name: string): { left: number, top: number, right: number, bottom: number };
 }
