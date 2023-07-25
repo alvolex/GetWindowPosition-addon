e3c04c4d46c84689b06f3038e5fe7f75a37687fb
alvolex <alexandervolkers@gmail.com>
2023-07-25 22:52:07 +0200

    ads possible fix for bad position values when multiple screens are connected

--- GetWindowPosition.cc
+++ GetWindowPosition.cc
@@ -11,13 +11,27 @@ Napi::Value GetWindowPositionByName(const Napi::CallbackInfo& info) {
 
     Napi::String name = info[0].As<Napi::String>();
 
-    const auto hWnd = FindWindowA(NULL, name.Utf8Value().c_str());
+    HWND hWnd = FindWindowA(NULL, name.Utf8Value().c_str());
     if (hWnd == NULL) {
         return env.Null();
     }
 
     RECT* lpRect = new RECT;
     GetWindowRect(hWnd, lpRect);
+
+    // Get DPI for the window
+    UINT dpi;
+    typedef UINT(WINAPI *GetDpiForWindow_t)(HWND);
+    GetDpiForWindow_t pGetDpiForWindow = (GetDpiForWindow_t)GetProcAddress(GetModuleHandle(TEXT("user32")), "GetDpiForWindow");
+    if (pGetDpiForWindow)
+    {
+        dpi = pGetDpiForWindow(hWnd);
+        // adjust the rect size by the dpi value
+        lpRect->left = MulDiv(lpRect->left, 96, dpi);
+        lpRect->top = MulDiv(lpRect->top, 96, dpi);
+        lpRect->right = MulDiv(lpRect->right, 96, dpi);
+        lpRect->bottom = MulDiv(lpRect->bottom, 96, dpi);
+    }
 
     Napi::Object result = Napi::Object::New(env);
     result.Set("left", Napi::Number::New(env, lpRect->left));
